5178844d24cf80c8d8ead7babaab257f29db2eee
Devyn Goetsch <devynmichellegoetsch@gmail.com>
2015-11-08 01:56:50 -0600

    Make controller methods asynchronous

--- app/controllers/GifController.scala
+++ app/controllers/GifController.scala
@@ -15,11 +15,13 @@ import akka.pattern.ask
 import play.api.libs.json.Json
 import play.api.data._
 import play.api.data.Forms._
-import scala.concurrent.Await
+import scala.concurrent.{Future, Await}
+
+case class DisplayRequest(name: String, width: Int, height: Int)
 
 @Singleton
-class EvilController @Inject() (@Named("gifReader") gifReader: ActorRef,
-                                 @Named("asciiRenderer") asciiRenderer: ActorRef) extends Controller {
+class GifController @Inject()(@Named("gifReader") gifReader: ActorRef,
+                              @Named("asciiRenderer") asciiRenderer: ActorRef) extends Controller {
   val GIFHTML = "<script type='text/javascript' src=\"/assets/javascript/render.js\"></script><div id=\"gifBody\"></div><div id=\"gifData\" hidden>%PLACEHOLDER%</div>"
   import scala.concurrent.ExecutionContext.Implicits.global
   implicit val timeout = Timeout(5000, TimeUnit.MILLISECONDS)
@@ -34,29 +36,22 @@ class EvilController @Inject() (@Named("gifReader") gifReader: ActorRef,
     Ok(views.html.index(displayRequest))
   }
 
-  def showGif = Action(parse.form(displayRequest)) { request =>
+  def showGif = Action.async(parse.form(displayRequest)) { request =>
     val displayRequest = request.body
-    Ok(views.html.gif(Json.toJson(addGif(displayRequest.name, displayRequest.width, displayRequest.height)).toString()))
-    //Ok(views.html(GIFHTML.replace("%PLACEHOLDER%", .toString())))
-//    var height: Int = if(request.queryString.get("height").isEmpty) 100 else request.queryString("height")(0).toInt
-//    var width: Int = if(request.queryString.get("width").isEmpty) 400 else request.queryString("width")(0).toInt
-//    var gifToUse = if(request.queryString.get("gif").isEmpty) "bb8" else request.queryString("gif")(0)
-
-    //Ok(Json.toJson(addGif(gifToUse, width, height)))
+    addGif(displayRequest.name, displayRequest.width, displayRequest.height).map { transformResult =>
+      Ok(views.html.gif(Json.toJson(transformResult.frames).toString()))
+    }
   }
 
-  def addGif(fileName: String, targetWidth: Int, targetHeight: Int): Seq[String] = {
+  def addGif(fileName: String, targetWidth: Int, targetHeight: Int): Future[TransformResult] = {
     var qualifiedName = if(fileName.startsWith("public/")) fileName else "public/" + fileName
     qualifiedName = if(qualifiedName.endsWith(".gif")) qualifiedName else qualifiedName + ".gif"
     var is: InputStream = Play.classloader.getResourceAsStream(qualifiedName)
     if(is == null) {
       is = new FileInputStream(fileName)
     }
-    val framesF = gifReader.ask(DoRead(is, targetWidth, targetHeight)).mapTo[TransformResult]
-    //await frames
-    Await.result(framesF, timeout.duration).frames
+    gifReader.ask(DoRead(is, targetWidth, targetHeight)).mapTo[TransformResult]
   }
 
 
 }
-case class DisplayRequest(name: String, width: Int, height: Int)
